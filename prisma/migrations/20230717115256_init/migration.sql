e65c35d08fb050982d2f03e8abd205b064169e48
jspark2000 <idpjs2000@gmail.com>
2023-07-17 11:53:21 +0000

    fix: rebase db schema

--- prisma/migrations/20230717115256_init/migration.sql
+++ prisma/migrations/20230717115256_init/migration.sql
@@ -4,11 +4,31 @@ CREATE TYPE "Role" AS ENUM ('Tutor', 'Student');
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "nickname" VARCHAR(255) NOT NULL,
     "role" "Role" NOT NULL,
+    "otpKey" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Repo" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "url" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Repo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRepo" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "repoId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserRepo_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -16,8 +36,8 @@ CREATE TABLE "Score" (
     "id" SERIAL NOT NULL,
     "score" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL,
-    "userId" INTEGER,
-    "problemId" INTEGER,
+    "userId" INTEGER NOT NULL,
+    "problemId" INTEGER NOT NULL,
 
     CONSTRAINT "Score_pkey" PRIMARY KEY ("id")
 );
@@ -27,6 +47,7 @@ CREATE TABLE "Problem" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "text" TEXT NOT NULL,
+    "repoId" INTEGER NOT NULL,
 
     CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
 );
@@ -36,7 +57,7 @@ CREATE TABLE "TestCase" (
     "id" SERIAL NOT NULL,
     "input" TEXT NOT NULL,
     "output" TEXT NOT NULL,
-    "problemId" INTEGER,
+    "problemId" INTEGER NOT NULL,
 
     CONSTRAINT "TestCase_pkey" PRIMARY KEY ("id")
 );
@@ -46,19 +67,31 @@ CREATE TABLE "HiddenCase" (
     "id" SERIAL NOT NULL,
     "input" TEXT NOT NULL,
     "output" TEXT NOT NULL,
-    "problemId" INTEGER,
+    "problemId" INTEGER NOT NULL,
 
     CONSTRAINT "HiddenCase_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
+
+-- AddForeignKey
+ALTER TABLE "UserRepo" ADD CONSTRAINT "UserRepo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRepo" ADD CONSTRAINT "UserRepo_repoId_fkey" FOREIGN KEY ("repoId") REFERENCES "Repo"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Score" ADD CONSTRAINT "Score_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Score" ADD CONSTRAINT "Score_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Problem" ADD CONSTRAINT "Problem_repoId_fkey" FOREIGN KEY ("repoId") REFERENCES "Repo"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TestCase" ADD CONSTRAINT "TestCase_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
