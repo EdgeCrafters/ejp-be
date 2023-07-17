f82d0b0a9b3af763e780b1de58f2b1c6e8eb3a20
jspark2000 <idpjs2000@gmail.com>
2023-07-17 09:17:35 +0000

    init commit

---
+++ scripts/setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Check requirements: npm
+if [ ! $(command -v npm) ]
+then
+  echo "Error: npm is not installed. Please install npm first."
+  exit 1
+fi
+
+BASEDIR=$(dirname $(dirname $(realpath $0)))
+
+cd $BASEDIR
+
+# Generate env file
+echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5443/skku?schema=public\"" >> .env
+
+# Install pnpm and Node.js packages
+npm install -g pnpm@latest
+pnpm install
+
+# Enable git auto completion
+if ! grep -q "bash-completion/completions/git" ~/.bashrc
+then
+  echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
+fi
+
+# Apply database migration
+for i in {1..5}
+do
+  pnpm exec prisma migrate dev && break # break if migration succeed
+  echo -e '\n⚠️ Failed to migrate. Waiting for db to be ready...\n'
+  sleep 5
+done
