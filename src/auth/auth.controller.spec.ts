1e5e7c457802fd8c324604bc21539cfbef726c4f
jspark2000 <idpjs2000@gmail.com>
2023-07-22 11:09:18 +0000

    fix: test codes

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,13 +1,16 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { AuthController } from './auth.controller'
 import { expect } from 'chai'
+import { AuthService } from './auth.service'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 describe('AuthController', () => {
   let controller: AuthController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController]
+      controllers: [AuthController],
+      providers: [AuthService, { provide: PrismaService, useValue: {} }]
     }).compile()
 
     controller = module.get<AuthController>(AuthController)
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { AuthService } from './auth.service'
 import { expect } from 'chai'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 describe('AuthService', () => {
   let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [AuthService, { provide: PrismaService, useValue: {} }]
     }).compile()
 
     service = module.get<AuthService>(AuthService)
