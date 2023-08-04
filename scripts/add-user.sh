b0e781765253985ec0b25668c0279f2adddf56af
Your Name <you@example.com>
2023-08-04 09:16:32 +0000

    fix: user grouping을 위한 script 수정

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -9,7 +9,7 @@ secret_key3=$5
 cd ./gitolite-admin/keydir/
 echo "$secret_key1 $secret_key2 $secret_key3" >> $user_name.pub 
 cd ../conf/groups
-echo "$user_name " > $repo_name.conf
+echo -n "$user_name " >> $repo_name.conf
 git add .
 git commit -m "$user_name user added"
 git push
