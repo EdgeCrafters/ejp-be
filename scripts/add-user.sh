1ac2b75cbf82706f78edc8e5d8e564fba98049ad
Your Name <you@example.com>
2023-08-11 09:06:21 +0000

    fix: git push 에러 수정

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -2,12 +2,7 @@
 
 user_name=$1
 repo_name=$2
-# secret_key1=$3
-# secret_key2=$4
-# secret_key3=$5
 
-# cd ./gitolite-admin/keydir/
-# echo "$secret_key1 $secret_key2 $secret_key3" >> $user_name.pub 
 cd ./gitolite-admin/conf/groups
 echo -n "$user_name " >> $repo_name.conf
 git add .
