86c62e9ccc97c58572eb2d14cd7f3895b4269c9d
Hanjunhee-1 <hanjuni857@naver.com>
2023-08-12 09:26:51 +0000

    feat: submit 로직 개발

--- src/submit/submit.controller.ts
+++ src/submit/submit.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+  Req,
+  UseGuards
+} from '@nestjs/common'
+import { SubmitService } from './submit.service'
+import { AuthenticatedGuard } from 'src/auth/guard/authenticated.guard'
 
 @Controller('submit')
-export class SubmitController {}
+export class SubmitController {
+  constructor(private readonly submitService: SubmitService) {}
+
+  @Get(':hiddenCaseId')
+  async getBias(@Param('hiddenCaseId', ParseIntPipe) hiddenCaseId: number) {
+    const bias = await this.submitService.getBias(hiddenCaseId)
+
+    return { bias: bias }
+  }
+
+  @Put(':hiddenCaseId')
+  @UseGuards(AuthenticatedGuard)
+  async updateResult(
+    @Param('hiddenCaseId', ParseIntPipe) hiddenCaseId: number,
+    @Body('hashedOutput') hashedOutput: string,
+    @Req() request
+  ) {
+    await this.submitService.updateResult(hiddenCaseId, hashedOutput, request)
+  }
+}
--- src/submit/submit.service.ts
+++ src/submit/submit.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class SubmitService {}
+export class SubmitService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getBias(hiddenCaseId: number) {
+    return (await this.getHiddenCase(hiddenCaseId)).bias
+  }
+
+  async updateResult(hiddenCaseId: number, hashedOutput: string, request) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: request.session.passport.user
+      }
+    })
+
+    if (!user) {
+      throw new BadRequestException('존재하지 않는 user 입니다')
+    }
+
+    const hiddenCase = await this.getHiddenCase(hiddenCaseId)
+
+    const userHiddenCase = await this.prismaService.userHiddenCase.findFirst({
+      where: {
+        userId: user.id,
+        hiddenCaseId: hiddenCase.id
+      }
+    })
+
+    try {
+      await this.prismaService.$transaction(async (tx) => {
+        if (userHiddenCase) {
+          // 나중에 hiddenCase 와 사용자의 hashedOutput 을 비교하는 부분 필요
+          await tx.userHiddenCase.update({
+            where: {
+              id: userHiddenCase.id
+            },
+            data: {
+              isCorrect: hiddenCase.output === hashedOutput ? true : false
+            }
+          })
+        } else {
+          await tx.userHiddenCase.create({
+            data: {
+              userId: user.id,
+              hiddenCaseId: hiddenCase.id,
+              isCorrect: hiddenCase.output === hashedOutput ? true : false
+            }
+          })
+        }
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(error)
+    }
+  }
+
+  private async getHiddenCase(hiddenCaseId: number) {
+    const hiddenCase = await this.prismaService.hiddenCase.findUnique({
+      where: {
+        id: hiddenCaseId
+      }
+    })
+
+    if (!hiddenCase) {
+      throw new BadRequestException('존재하지 않는 hiddenCase 입니다.')
+    }
+
+    return hiddenCase
+  }
+}
