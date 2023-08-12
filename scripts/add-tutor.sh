616690b567a86efd7772ef7af82e56c82b898831
Your Name <you@example.com>
2023-08-12 12:14:31 +0000

    fix: add-tutor에서 atomic하게 회원가입하게 수정

--- scripts/add-tutor.sh
+++ scripts/add-tutor.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 user_name=$1
+secret_key1=$2
+secret_key2=$3
+secret_key3=$4
 
 cd ./gitolite-admin/conf/groups
-echo -e "$user_name " >> tutors.conf
+echo -n "$user_name " >> tutors.conf
+cd ../../keydir
+echo "$secret_key1 $secret_key2 $secret_key3" >> $user_name.pub 
 git add .
 git commit -m "$user_name tutor added"
 git push
