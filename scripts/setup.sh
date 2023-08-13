5c4b84e4ee7551290d9804a933dfff7b4f9993d7
Your Name <you@example.com>
2023-08-13 14:38:56 +0000

    fix: setup.sh에서 gitolite관련 설정 삭제

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,46 +36,14 @@ do
   sleep 5
 done
 
-echo installing ssh-server settings..
-sudo apt-get update
-sudo apt-get install -y openssh-server ssh-client
-service ssh restart
-yes '' | ssh-keygen -N ''
-
-echo setting git user..
-usernames=("git" "testuser")
-for username in "${usernames[@]}"
-do
-sudo useradd -m -s /bin/bash "$username"
-
-echo "$username:0000" | sudo chpasswd
-
-sudo chown -R "$username:$username" "/home/$username"
-
-sudo chmod 700 "/home/$username"
-done
-
-rm -rf gitolite-admin
-cp /root/.ssh/id_rsa.pub /tmp/root.pub
-ssh git@localhost -t 'git clone https://github.com/sitaramc/gitolite && cd $HOME && mkdir -p bin && gitolite/install -to $HOME/bin && cd $HOME && $HOME/bin/gitolite setup -pk /tmp/root.pub && exit; bash'
-
 echo "chmod to scripts.."
 chmod +x ./scripts/create-new-repo.sh
 chmod +x ./scripts/add-user.sh
 chmod +x ./scripts/add-tutor.sh
 chmod +x ./scripts/create-user.sh
+chmod +x ./scripts/gitolite-init.sh
 
-
-echo "cloning gitotlie-admin to backend.."
-git clone git@localhost:gitolite-admin
-cd gitolite-admin
-mkdir ./conf/groups
-echo 'include "groups/*.conf"' >> ./conf/gitolite.conf
-echo -n '@tutors = ' >> ./conf/groups/tutors.conf
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-git add . && git commit -m "group conf-file added" && git push
-cd ..
+git config --global --add safe.directory /workspace
 
 echo "setup scripts for ejs-t & ejs-s"
 cd $BASEDIR
