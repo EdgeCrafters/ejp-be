76f813a12dc3bfba8e35a3bd74e4045490255e49
chanyeong <jcy030896@naver.com>
2023-08-16 02:55:21 +0000

    feat: testcase, hiddencase를 추가하는 api 작성

--- src/testcase/testcase.controller.ts
+++ src/testcase/testcase.controller.ts
@@ -5,14 +5,21 @@ import {
   Param,
   ParseIntPipe,
   Patch,
+  Post,
   UseGuards
 } from '@nestjs/common'
 import { TestcaseService } from './testcase.service'
 import { Roles } from 'src/common/decorator/roles.decorator'
 import { type HiddenCase, Role, type TestCase } from '@prisma/client'
 import { TestcaseGuard } from './guards/testcase.guard'
-import { UpdateHiddencaseDTO, UpdateTestcaseDTO } from './dto/testcase.dto'
+import {
+  CreateHiddencaseDto,
+  CreateTestcaseDto,
+  UpdateHiddencaseDTO,
+  UpdateTestcaseDTO
+} from './dto/testcase.dto'
 import { HiddencaseGuard } from './guards/hiddencase.guard'
+import { CommonResponseDto } from 'src/common/dtos/common-response.dto'
 
 @Roles(Role.Tutor)
 @Controller('testcase')
@@ -54,5 +61,19 @@ export class TestcaseController {
     @Param('hiddencaseId', ParseIntPipe) hiddencaseId: number
   ): Promise<HiddenCase> {
     return await this.testcaseService.deleteHiddencase(hiddencaseId)
+  }
+
+  @Roles(Role.Tutor)
+  @Post('/testcase')
+  async createTestCase(@Body() createTestcaseDto: CreateTestcaseDto) {
+    await this.testcaseService.createTestcase(createTestcaseDto)
+    return new CommonResponseDto()
+  }
+
+  @Roles(Role.Tutor)
+  @Post('/hiddencase')
+  async createHiddencase(@Body() createHiddencaseDto: CreateHiddencaseDto) {
+    await this.testcaseService.createHiddencase(createHiddencaseDto)
+    return new CommonResponseDto()
   }
 }
