9da51e6065b293ee8d490b84fee853fbf7bb4fe0
JungangHighschool <lhs109802@g.skku.edu>
2023-08-18 22:04:25 +0900

    hot-fix : creating user repo

--- prisma/migrations/20230811131938_add_is_correct_field_on_user_hidden_case/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "UserHiddenCase" ADD COLUMN     "isCorrect" BOOLEAN;
--- prisma/migrations/20230817124452_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Problem" ADD COLUMN     "uuid" VARCHAR(255) NOT NULL DEFAULT 'null';
