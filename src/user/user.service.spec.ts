127050d6ba889d8345c7873b1f8a62e873292e89
chanyeong <jcy030896@naver.com>
2023-08-23 11:31:12 +0000

    fix: spec.ts 또 수정

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserService } from './user.service'
 import { expect } from 'chai'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 describe('UserService', () => {
   let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService]
+      providers: [UserService, { provide: PrismaService, useValue: {} }]
     }).compile()
-
     service = module.get<UserService>(UserService)
   })
 
