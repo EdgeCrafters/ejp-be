21036c5f8d2aaffcce9ecffaff509ab5b6cb7400
chanyeong <jcy030896@naver.com>
2023-08-23 11:28:05 +0000

    fix: spec.ts 수정

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from './user.controller'
 import { expect } from 'chai'
+import { UserService } from './user.service'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 describe('UserController', () => {
   let controller: UserController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController]
+      controllers: [UserController],
+      providers: [UserService, { provide: PrismaService, useValue: {} }]
     }).compile()
 
     controller = module.get<UserController>(UserController)
