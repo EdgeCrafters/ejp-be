21243d0e3e706f75efeab37fbba85c6200a2c571
jspark2000 <idpjs2000@gmail.com>
2023-08-23 13:16:07 +0000

    feat: implements submit service and controller

--- src/submit/submit.controller.ts
+++ src/submit/submit.controller.ts
@@ -8,6 +8,14 @@ import { AuthenticatedRequest } from 'src/common/interface/authenticated-request
 export class SubmitController {
   constructor(private readonly submitService: SubmitService) {}
 
+  @Get('result/problemId/:problemId')
+  async getMyScore(
+    @Param('problemId', ParseIntPipe) problemId: number,
+    @Req() req: AuthenticatedRequest
+  ): Promise<Score> {
+    return await this.submitService.getScore(req.user.userId, problemId)
+  }
+
   @Get('result/userId/:userId/problemId/:problemId')
   @Roles(Role.Tutor)
   async getStudentScore(
@@ -17,11 +25,21 @@ export class SubmitController {
     return await this.submitService.getScore(userId, problemId)
   }
 
-  @Get('result/problemId/:problemId')
-  async getScore(
+  @Get('result/all/userId/:userId/reopId/:reopId')
+  @Roles(Role.Tutor)
+  async getStudentScores(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Param('reopId', ParseIntPipe) reopId: number
+  ): Promise<Score[]> {
+    return await this.submitService.getRepoScores(userId, reopId)
+  }
+
+  @Get('result/all/problemId/:problemId/reopId/:reopId')
+  @Roles(Role.Tutor)
+  async getRepoProblemScores(
     @Param('problemId', ParseIntPipe) problemId: number,
-    @Req() req: AuthenticatedRequest
-  ): Promise<Score> {
-    return await this.submitService.getScore(req.user.userId, problemId)
+    @Param('reopId', ParseIntPipe) repoId: number
+  ): Promise<Score[]> {
+    return await this.submitService.getRepoProblemScores(problemId, repoId)
   }
 }
